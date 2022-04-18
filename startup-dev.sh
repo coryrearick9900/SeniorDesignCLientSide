43bc7816548f930ba27cf7b3cc453e585e25241a
Cory Rearick <58087349+coryrearick9900@users.noreply.github.com>
2022-04-18 18:00:29 -0400

    A

--- startup-dev.sh
+++ startup-dev.sh
@@ -1,12 +1,9 @@
 echo "Starting data collection system..."
 
-docker network create test
+# docker network create test
 docker run -it --rm --network test --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.9-management &
 
 cd ./frontend
 npm run dev &
 cd ..
 python ./manage.py runserver
-
-
-# docker run -it --rm --network test --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.9-management &
